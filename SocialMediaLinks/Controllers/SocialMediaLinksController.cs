0e9caad5159d2c9d2a67d398160250d0e1da6af2
comfygrapes <76193542+comfygrapes@users.noreply.github.com>
2024-02-27 21:45:37 +0800

    Add project files.

---
+++ SocialMediaLinks/Controllers/SocialMediaLinksController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using SocialMediaLinks.Options;
+
+namespace SocialMediaLinks.Controllers
+{
+    public class SocialMediaLinksController : Controller
+    {
+        private readonly SocialMediaLinksOptions _socialMediaLinksOptions;
+
+        public SocialMediaLinksController(IOptions<SocialMediaLinksOptions> options)
+        {
+            _socialMediaLinksOptions = options.Value;
+        }
+
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewData["FacebookLink"] = _socialMediaLinksOptions.Facebook;
+            ViewData["InstagramLink"] = _socialMediaLinksOptions.Instagram;
+            ViewData["TwitterLink"] = _socialMediaLinksOptions.Twitter;
+            ViewData["YoutubeLink"] = _socialMediaLinksOptions.Youtube;
+
+            return View();
+        }
+    }
+}
